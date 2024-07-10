59b51fe4756dc879156c7d6a9617d18cbabd1604
asunder24 <ajiteshsunder24@gmail.com>
2024-07-09 21:41:53 -0700

    Adding operator scanning

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -34,21 +34,64 @@ class Scanner{
     private void scanToken(){
         char c = advance();
         switch (c){
-            case '(': addToken(LEFT_PAREN); break;
-            case ')': addToken(RIGHT_PAREN); break;
-            case '{': addToken(LEFT_BRACE); break;
-            case '}': addToken(RIGHT_BRACE); break;
-            case ',': addToken(COMMA); break;
-            case '.': addToken(DOT); break;
-            case '-': addToken(MINUS); break;
-            case '+': addToken(PLUS); break;
-            case ';': addToken(SEMICOLON); break;
-            case '*': addToken(STAR); break;
+            case '(': 
+                addToken(LEFT_PAREN); 
+                break;
+            case ')': 
+                addToken(RIGHT_PAREN); 
+                break;
+            case '{': 
+                addToken(LEFT_BRACE); 
+                break;
+            case '}': 
+                addToken(RIGHT_BRACE); 
+                break;
+            case ',': 
+                addToken(COMMA); 
+                break;
+            case '.': 
+                addToken(DOT); 
+                break;
+            case '-': 
+                addToken(MINUS); 
+                break;
+            case '+': 
+                addToken(PLUS); 
+                break;
+            case ';': 
+                addToken(SEMICOLON); 
+                break;
+            case '*': 
+                addToken(STAR); 
+                break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
 
             default:
                 Lox.error(line, "unexpected character");
                 break;
         }
+    }
+
+    private boolean match(char expected){
+        if (isAtEnd()){
+            return false;
+        }
+        if (source.charAt(current) != expected){
+            return false;
+        }
+        current++;
+        return true;
     }
 
     private char advance(){
