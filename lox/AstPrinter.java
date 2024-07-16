3316bca18e6bbc97fef27774c4e71ecd717728d4
asunder24 <ajiteshsunder24@gmail.com>
2024-07-16 14:54:33 -0700

    Adds parenthesizing to printer

--- lox/AstPrinter.java
+++ lox/AstPrinter.java
@@ -30,6 +30,12 @@ class AstPrinter implements Expr.Visitor<String>{
 
     private String parenthesize(String name, Expr... exprs){
         StringBuilder builder = new StringBuilder();
-        
+        builder.append("(").append(name);
+        for (Expr expr : exprs){
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+        return builder.toString();
     }
 }
