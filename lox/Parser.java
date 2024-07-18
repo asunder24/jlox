6f1c4d418d31051e5517aa05450986dde36ca6bd
asunder24 <ajiteshsunder24@gmail.com>
2024-07-17 22:49:02 -0700

    Adding initial rules

--- lox/Parser.java
+++ lox/Parser.java
@@ -10,4 +10,34 @@ class Parser{
     Parser(List<Token> tokens){
         this.tokens = tokens;
     }
+
+    private Expr expression(){
+        return equality();
+    }
+
+    private Expr equality(){
+        Expr expr = comparison();
+
+        while(match(BANG_EQUAL, EQUAL_EQUAL)){
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private boolean match(TokenType... types){
+        for (TokenType type : types){
+            if (check(type)){
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    
+
+
 }
