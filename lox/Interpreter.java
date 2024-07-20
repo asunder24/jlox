a8cc80c7d6e97cedb5c683153f45ac9df00a8be5
asunder24 <ajiteshsunder24@gmail.com>
2024-07-20 16:50:36 -0700

    Parentheses and literals

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -1,3 +1,17 @@
 package lox;
 
+class Interpreter implements Expr.Visitor<Object>{
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr){
+        return expr.value;
+    }
 
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr){
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr){
+        return expr.accept(this);
+    }
+}
