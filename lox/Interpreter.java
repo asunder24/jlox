6c1bf9f0cb4d7c12ccc4cd035a33499b8a05bdf9
asunder24 <ajiteshsunder24@gmail.com>
2024-07-21 14:11:41 -0700

    Error handling for binary expressions

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -41,10 +41,13 @@ class Interpreter implements Expr.Visitor<Object>{
         Object right = evaluate(expr.right);
         switch(expr.operator.type){
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left - (double)right;
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left / (double)right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left * (double)right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double){
@@ -53,14 +56,18 @@ class Interpreter implements Expr.Visitor<Object>{
                 else if (left instanceof String && right instanceof String){
                     return (String)left + (String)right;
                 }
-                break;
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double)right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left >= (double)right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double)right;
-            case LESS_EQUAL: 
+            case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left <= (double)right;
             case BANG_EQUAL:
                 return !isEqual(left, right);
@@ -69,6 +76,13 @@ class Interpreter implements Expr.Visitor<Object>{
         }
 
         return null;
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right){
+        if (left instanceof Double && right instanceof Double){
+            return;
+        }
+        throw new RuntimeError(operator, "Operands must be numbers.");
     }
 
     private boolean isEqual(Object left, Object right){
