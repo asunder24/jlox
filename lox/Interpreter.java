6f613036a74bdeb3c419de138d30d3a73fc76572
asunder24 <ajiteshsunder24@gmail.com>
2024-07-21 13:07:47 -0700

    Evaluate binary operations

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -27,6 +27,53 @@ class Interpreter implements Expr.Visitor<Object>{
         return null;
     }
 
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr){
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+        switch(expr.operator.type){
+            case MINUS:
+                return (double)left - (double)right;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double){
+                    return (double)left + (double)right;
+                }
+                else if (left instanceof String && right instanceof String){
+                    return (String)left + (String)right;
+                }
+                break;
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL: 
+                return (double)left <= (double)right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+        }
+
+        return null;
+    }
+
+    private boolean isEqual(Object left, Object right){
+        if (left == null && right == null){
+            return true;
+        }
+        if (left == null){
+            return false;
+        }
+        return left.equals(right);
+
+    }
+    
     private boolean isTruthy(Object object){
         if (object == null){
             return false;
@@ -36,4 +83,6 @@ class Interpreter implements Expr.Visitor<Object>{
         }
         return true;
     }
+
+    
 }
