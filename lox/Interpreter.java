e90493fb63514041fd6b72730b757d28a773e628
asunder24 <ajiteshsunder24@gmail.com>
2024-07-20 22:26:28 -0700

    Unary expressions and truthy/falsey implementation

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -14,4 +14,26 @@ class Interpreter implements Expr.Visitor<Object>{
     private Object evaluate(Expr expr){
         return expr.accept(this);
     }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr){
+        Object right = evaluate(expr.right);
+        switch(expr.operator.type){
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+        return null;
+    }
+
+    private boolean isTruthy(Object object){
+        if (object == null){
+            return false;
+        }
+        if (object instanceof boolean){
+            return (boolean)object;
+        }
+        return true;
+    }
 }
