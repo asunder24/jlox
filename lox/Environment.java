f07ad8085fd307a926ebf005e7a6c1523031ed9e
asunder24 <ajiteshsunder24@gmail.com>
2024-07-26 16:10:38 -0700

    Allows redefinition of global variables

--- lox/Environment.java
+++ lox/Environment.java
@@ -6,5 +6,7 @@ import java.util.Map;
 class Environment{
     private final Map<String, Object> values = new HashMap<>();
 
-
+    void define(String name, Object value){
+        values.put(name, value);
+    }
 }
