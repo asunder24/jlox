12ca9338e6f61e1cd651559de9068b6879617857
asunder24 <ajiteshsunder24@gmail.com>
2024-07-26 22:00:18 -0700

    Interpret global variables

--- lox/Environment.java
+++ lox/Environment.java
@@ -9,4 +9,11 @@ class Environment{
     void define(String name, Object value){
         values.put(name, value);
     }
+
+    Object get(Token name){
+        if (values.containsKey(name.lexeme)){
+            return values.get(name.lexeme);
+        }
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
 }
