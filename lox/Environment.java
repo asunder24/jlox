a44fcd33a900b25ac2482d46159431329757fff3
asunder24 <ajiteshsunder24@gmail.com>
2024-07-28 16:47:34 -0700

    Adds constructors for nested environments

--- lox/Environment.java
+++ lox/Environment.java
@@ -5,6 +5,15 @@ import java.util.Map;
 
 class Environment{
     private final Map<String, Object> values = new HashMap<>();
+    final Environment enclosing;
+
+    Environment(){
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing){
+        this.enclosing = enclosing;
+    }
 
     void define(String name, Object value){
         values.put(name, value);
