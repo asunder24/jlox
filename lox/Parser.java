981da12ed657cbc320e0cda7dadd5af9836743de
asunder24 <ajiteshsunder24@gmail.com>
2024-07-31 23:23:26 -0700

    Complete function parsing

--- lox/Parser.java
+++ lox/Parser.java
@@ -191,6 +191,9 @@ class Parser{
             while(match(COMMA))
         }
         consume(RIGHT_PAREN, "Expect ')' after parameters.");
+        consume(LEFT_BRACE, "Expect '{' before " + kind + " body.");
+        List<Stmt> body = block();
+        return new Stmt.Function(name, parameters, body);
     }
 
     private Stmt varDeclaration(){
