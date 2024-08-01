5d7d7fc10a00c6a3103f1bf69efb4d6800912e51
asunder24 <ajiteshsunder24@gmail.com>
2024-07-31 17:29:42 -0700

    Add error checking for valid functions

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -189,12 +189,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     }
 
     @Override
-    public Object visitCallExpr(Exprr.Call expr){
+    public Object visitCallExpr(Expr.Call expr){
         Object callee = evaluate(expr.callee);
         List<Object> arguments = new ArrayList<>();
         for(Expr argument: expr.arguments){
             arguments.add(evaluate(argument));
         }
+        if(!(callee instanceof LoxCallable)){
+            throw new RuntimeError(expr.paren, "Can only call functions and classes.");
+        }
+
         LoxCallable function = (LoxCallable)callee;
         return function.call(this, arguments);
     }
