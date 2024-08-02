c4cdbd5c02e978b3b43938cb5fe502d9e7c54f1c
asunder24 <ajiteshsunder24@gmail.com>
2024-08-02 16:25:09 -0700

    Interpret function return statements

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -129,6 +129,15 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     }
 
     @Override
+    public Void visitReturnStmt(Stmt.Return stmt){
+        Object value = null;
+        if (stmt.value != null){
+            value = evaluate(stmt.value);
+        }
+        throw new Return(value);
+    }
+
+    @Override
     public Void visitIfStmt(Stmt.If stmt){
         if (isTruthy(evaluate(stmt.condition))){
             execute(stmt.thenBranch);
