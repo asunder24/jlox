74493b3059ec10c42bc66e59928f18925e5252f4
asunder24 <ajiteshsunder24@gmail.com>
2024-08-04 16:58:25 -0700

    Resolve variable expressions

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -31,6 +31,24 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         return null;
     }
 
+    @Override
+    public Void visitVariableExpr(Expr.Variable expr){
+        if (!scopes.isEmpty() &&scopes.peek().get(expr.name.lexeme) == Boolean.FALSE){
+            Lox.error(expr.name,"Can't read local variable in its own initializer.");
+        }
+    resolveLocal(expr, expr.name);
+    return null;
+    }
+
+    private void resolveLocal(Expr expr, Token name){
+        for (int i = scopes.size() - 1; i >= 0; i--){
+        if (scopes.get(i).containsKey(name.lexeme)){
+            interpreter.resolve(expr, scopes.size() - 1 - i);
+            return;
+            }
+        }
+    }
+
     private void declare(Token name){
         if (scopes.isEmpty()){
             return;
