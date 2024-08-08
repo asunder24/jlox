47da8e2056ec4f39686257ff6c438f1df7971519
asunder24 <ajiteshsunder24@gmail.com>
2024-08-08 16:09:21 -0700

    Parse class syntax

--- lox/Parser.java
+++ lox/Parser.java
@@ -176,6 +176,9 @@ class Parser{
 
     private Stmt declaration(){
         try{
+            if (match(CLASS)){
+                return classDeclaration();
+            }
             if (match(FUN)){
                 return function("function");
             }
@@ -188,6 +191,17 @@ class Parser{
             synchronize();
             return null;
         }
+    }
+
+    private Stmt classDeclaration(){
+        Token name = consume(IDENTIFIER, "Expect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+        List<Stmt.Function> methods = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()){
+            methods.add(function("method"));
+        }
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+        return new Stmt.Class(name, methods);
     }
 
     private Stmt.Function function(String kind){
