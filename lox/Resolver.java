948b334582966538c7c942c44c81e8e9458daf98
asunder24 <ajiteshsunder24@gmail.com>
2024-08-20 15:47:48 -0700

    Call superclass methods -- all basic functionality complete

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -23,7 +23,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
     private ClassType currentClass = ClassType.NONE;
 
@@ -81,7 +82,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
             Lox.error(stmt.superclass.name, "A class can't inherit from itself.");
         }
         if (stmt.superclass != null){
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
+        }
+        if (stmt.superclass != null){
+            beginScope();
+            scopes.peek().put("super", true);
         }
         beginScope();
         scopes.peek().put("this", true);
@@ -93,7 +99,22 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
             resolveFunction(method, declaration); 
         }
         endScope();
+        if (stmt.superclass != null){
+            endScope();
+        }
         currentClass = enclosingClass;
+        return null;
+    }
+
+    @Override
+    public Void visitSuperExpr(Expr.Super expr){
+        if (currentClass == ClassType.NONE){
+            Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+        } 
+        else if (currentClass != ClassType.SUBCLASS){
+            Lox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+        }
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
